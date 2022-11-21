837aa17cebdff491081a71669c861f178b6af189
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-11-21 15:06:55 +0700

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:after_uts/login.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "/": (context) => const Login(),
+      },
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        fontFamily: 'Nunito',
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
